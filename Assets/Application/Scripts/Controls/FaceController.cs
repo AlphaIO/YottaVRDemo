647e11c5e11d53859d7b57c32ad9127569e89ff4
Prorokus <prorokivasik@gmail.com>
2017-01-31 14:40:51 +0200

    Bluetooth High Rate reading - 1h Bar Graphs of Degree of Activation (charts lines implementation) - 1h 30m

--- Assets/Application/Scripts/Controls/FaceController.cs
+++ Assets/Application/Scripts/Controls/FaceController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using MORPH3D;
 using MORPH3D.FOUNDATIONS;
@@ -10,13 +11,58 @@ public class FaceController : EmotionData {
     [SerializeField]
     Presentation SlideShow;
 
-    public void SetEmotion(string emotion) {
-        webAPI.AddState(emotion);
-        SlideShow.SetEmotionCountText(emotion);
-		StartChangeFace (emotion, .4f);
-    }
+	public Image neutralBar;
+	public Image surpriseBar;
+	public Image smileBar;
+	public Image contemptBar;
+	public Image disgustBar;
+	public Image sadnessBar;
+	public Image angerBar;
 
-    public void SetFace(string feeling) {
-        SetEmotion(feeling);
+	private const float defaultFullWidth = 140.0f;
+	private const float defaultMinWidth = 1.0f;
+
+    public void SetEmotion(string[] emotion) {
+        //webAPI.AddState(emotion);
+        //SlideShow.SetEmotionCountText(emotion);
+
+		//ToDo This is temporary here
+		//Will be changed soon
+		neutralBar.rectTransform.sizeDelta = new Vector2 (defaultMinWidth, 24);
+		surpriseBar.rectTransform.sizeDelta = new Vector2 (defaultMinWidth, 24);
+		smileBar.rectTransform.sizeDelta = new Vector2 (defaultMinWidth, 24);
+		contemptBar.rectTransform.sizeDelta = new Vector2 (defaultMinWidth, 24);
+		disgustBar.rectTransform.sizeDelta = new Vector2 (defaultMinWidth, 24);
+		sadnessBar.rectTransform.sizeDelta = new Vector2 (defaultMinWidth, 24);
+		angerBar.rectTransform.sizeDelta = new Vector2 (defaultMinWidth, 24);
+
+		float emotionLevelWidth = float.Parse (emotion [1]) / 10 * defaultFullWidth;
+
+		switch (emotion[0])
+		{
+			case "neutral":
+				neutralBar.rectTransform.sizeDelta = new Vector2 (emotionLevelWidth, 24);
+				break;
+			case "smile":
+				smileBar.rectTransform.sizeDelta = new Vector2 (emotionLevelWidth, 24);
+				break;
+			case "surprise":
+				surpriseBar.rectTransform.sizeDelta = new Vector2 (emotionLevelWidth, 24);
+				break;
+			case "contempt":
+				contemptBar.rectTransform.sizeDelta = new Vector2 (emotionLevelWidth, 24);
+				break;
+			case "anger":
+				angerBar.rectTransform.sizeDelta = new Vector2 (emotionLevelWidth, 24);
+				break;
+			case "disgust":
+				disgustBar.rectTransform.sizeDelta = new Vector2 (emotionLevelWidth, 24);
+				break;
+			case "sadness":
+				sadnessBar.rectTransform.sizeDelta = new Vector2 (emotionLevelWidth, 24);
+				break;
+		}
+
+		StartChangeFace (emotion[0], .4f);
     }
 }
--- Assets/BtLibrary/Teensy_Unity/BluetoothListener.cs
+++ Assets/BtLibrary/Teensy_Unity/BluetoothListener.cs
@@ -6,7 +6,7 @@ using TechTweaking.Bluetooth;
 using UnityEngine.UI;
 
 [System.Serializable]
-public class BluetoothEvent : UnityEngine.Events.UnityEvent<string> { }
+public class BluetoothEvent : UnityEngine.Events.UnityEvent<string[]> { }
 
 public class BluetoothListener : MonoBehaviour {
 	public FaceController[] FaceControllerArray;
@@ -22,18 +22,18 @@ public class BluetoothListener : MonoBehaviour {
 
 	public const float DATA_UPDATE_FREQUENCY = 1.0f / 30.0f;
 
-	private char[] charsToTrim = { '{', '}' };
+	private char[] charsToTrim = { '{', '}', '(', ')' };
 	private string[] inputDataVariants = new string[] 
 	{
-		"{neutral, 0,0,0,0,0,0,0,0,0,0}",
-		"{smile, 0,0,0,0,0,0,0,0,0,0}",
-		"{surprise, 0,0,0,0,0,0,0,0,0,0}",
-		"{contempt, 0,0,0,0,0,0,0,0,0,0}"//,
+		"{neutral, 10,0,0,0,0,0,0,0,0,0}",
+		"{smile, 9,0,0,0,0,0,0,0,0,0}",
+		"{surprise, 8,0,0,0,0,0,0,0,0,0}",
+		"{contempt, 7,0,0,0,0,0,0,0,0,0}",
+		"{anger, 5,0,0,0,0,0,0,0,0,0}",
+		"{disgust, 3,0,0,0,0,0,0,0,0,0}",
+		"{sadness, 1,0,0,0,0,0,0,0,0,0}"//,
 
-		/*"{anger, 0,0,0,0,0,0,0,0,0,0}",
-		"{disgust, 0,0,0,0,0,0,0,0,0,0}",
-		"{fear, 0,0,0,0,0,0,0,0,0,0}",
-		"{sadness, 0,0,0,0,0,0,0,0,0,0}",
+		/*"{fear, 0,0,0,0,0,0,0,0,0,0}",
 		"{big_wide_smile, 0,0,0,0,0,0,0,0,0,0}",
 		"{closed_smile, 0,0,0,0,0,0,0,0,0,0}",
 		"{eyebrows_only, 0,0,0,0,0,0,0,0,0,0}",
@@ -46,7 +46,7 @@ public class BluetoothListener : MonoBehaviour {
 		"{teethview, 0,0,0,0,0,0,0,0,0,0}"*/
 	};
 
-	private string parsedFeeling;
+	private string[] parsedFeeling;
 
     void Awake() {
         device = new BluetoothDevice();
@@ -168,7 +168,7 @@ public class BluetoothListener : MonoBehaviour {
 		{
 			//SetDebugText ("device.IsReading");
 
-			if (device.IsDataAvailable) {
+			/*if (device.IsDataAvailable) {
 				//{smile, 0,0,0,0,0,0,0,0,0,0}
 				//SetDebugText ("device.IsDataAvailable");
 				byte [] msg = device.read ();//because we called setEndByte(10)..read will always return a packet excluding the last byte 10.
@@ -184,7 +184,38 @@ public class BluetoothListener : MonoBehaviour {
 				}
             }
 
-			yield return new WaitForSecondsRealtime (DATA_UPDATE_FREQUENCY);
+			yield return new WaitForSecondsRealtime (DATA_UPDATE_FREQUENCY);*/
+
+			//----------------High Rate Reading--------------
+			if (device.IsDataAvailable) {
+				//polll all available packets
+				BtPackets packets = device.readAllPackets ();
+
+				if (packets != null && packets.Count > 0) {
+
+					/*
+				 *  'packets' are ordered by indecies (0,1,2,3 ... N),
+				 * where Nth packet is the latest packet and 0th is the oldest/first arrived packet.
+				 * 
+				 */
+
+					/*
+				* Ignore all old Texts within a single frame 
+				*(for example the comming packets are ('angry', 'smily') where 'smily' is the last packet, the code will take 'smily')
+				* "because it's a single frame" 
+				*/
+
+					int indx = packets.get_packet_offset_index (packets.Count - 1);
+					int size = packets.get_packet_size (packets.Count - 1);
+
+					parsedFeeling = System.Text.ASCIIEncoding.ASCII.GetString (packets.Buffer, indx, size).Trim (charsToTrim).Split (',');
+					Debug.Log("BLUETOOTH : " + device.Name + " Feels : " + parsedFeeling[0]);
+					SetDebugText (parsedFeeling[0]);
+					onDataReceived.Invoke(parsedFeeling);
+				}
+
+				yield return new WaitForSecondsRealtime (DATA_UPDATE_FREQUENCY);;
+			}
         }
 
 		SetDebugText ("Done Reading");
@@ -204,10 +235,9 @@ public class BluetoothListener : MonoBehaviour {
 		while (true) {
 			yield return new WaitForSecondsRealtime (1.5f);
 
-			parsedFeeling = inputDataVariants[Random.Range (0, inputDataVariants.Length)].Trim (charsToTrim).Split (',') [0];
+			parsedFeeling = inputDataVariants[Random.Range (0, inputDataVariants.Length)].Trim (charsToTrim).Split (',');
+			SetDebugText (parsedFeeling[0]);
 			onDataReceived.Invoke (parsedFeeling);
-
-			SetDebugText (parsedFeeling);
 		}
 	}
 
@@ -234,69 +264,69 @@ public class BluetoothListener : MonoBehaviour {
 		//Debug input from keyboard
 		#if UNITY_EDITOR
 			if (Input.GetKeyDown (KeyCode.Alpha1)) {
-				parsedFeeling = inputDataVariants[0].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[0].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
-				parsedFeeling = inputDataVariants[1].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[1].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
-				parsedFeeling = inputDataVariants[2].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[2].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			} else if (Input.GetKeyDown (KeyCode.Alpha4)) {
-				parsedFeeling = inputDataVariants[3].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[3].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			} else if (Input.GetKeyDown (KeyCode.Alpha5)) {
-				parsedFeeling = inputDataVariants[4].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[4].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			} else if (Input.GetKeyDown (KeyCode.Alpha6)) {
-				parsedFeeling = inputDataVariants[5].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[5].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			} else if (Input.GetKeyDown (KeyCode.Alpha7)) {
-				parsedFeeling = inputDataVariants[6].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[6].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			} else if (Input.GetKeyDown (KeyCode.Alpha8)) {
-				parsedFeeling = inputDataVariants[7].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[7].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 
 			else if (Input.GetKeyDown (KeyCode.F1)) {
-				parsedFeeling = inputDataVariants[8].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[8].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F2)) {
-				parsedFeeling = inputDataVariants[9].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[9].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F3)) {
-				parsedFeeling = inputDataVariants[10].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[10].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F4)) {
-				parsedFeeling = inputDataVariants[11].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[11].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F5)) {
-				parsedFeeling = inputDataVariants[12].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[12].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F6)) {
-				parsedFeeling = inputDataVariants[13].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[13].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F7)) {
-				parsedFeeling = inputDataVariants[14].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[14].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F8)) {
-				parsedFeeling = inputDataVariants[15].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[15].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F9)) {
-				parsedFeeling = inputDataVariants[16].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[16].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 			else if (Input.GetKeyDown (KeyCode.F10)) {
-				parsedFeeling = inputDataVariants[17].Trim (charsToTrim).Split (',') [0];
+				parsedFeeling = inputDataVariants[17].Trim (charsToTrim).Split (',');
 				onDataReceived.Invoke (parsedFeeling);
 			}
 		#endif
